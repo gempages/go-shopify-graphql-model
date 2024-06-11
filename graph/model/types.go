6ea429a6e3e3571f093ab0e001fef0292180b8fc
TungSC <tungngo@gempages.help>
2024-06-11 14:03:11 +0700

    UnmarshalJSON for DiscountNode

--- graph/model/types.go
+++ graph/model/types.go
@@ -17,6 +17,20 @@ func concludeObjectType(typeName string) (reflect.Type, error) {
 		return reflect.TypeOf(ExternalVideo{}), nil
 	case "GenericFile":
 		return reflect.TypeOf(GenericFile{}), nil
+	case "DiscountAutomaticApp":
+		return reflect.TypeOf(DiscountAutomaticApp{}), nil
+	case "DiscountCodeBasic":
+		return reflect.TypeOf(DiscountCodeBasic{}), nil
+	case "DiscountCodeBuyXGetY":
+		return reflect.TypeOf(DiscountCodeBxgy{}), nil
+	case "DiscountCodeFreeShipping":
+		return reflect.TypeOf(DiscountCodeFreeShipping{}), nil
+	case "DiscountAutomaticBasic":
+		return reflect.TypeOf(DiscountAutomaticBasic{}), nil
+	case "DiscountAutomaticBxgy":
+		return reflect.TypeOf(DiscountAutomaticBxgy{}), nil
+	case "DiscountAutomaticFreeShipping":
+		return reflect.TypeOf(DiscountAutomaticFreeShipping{}), nil
 	default:
 		return reflect.TypeOf(nil), fmt.Errorf("`%s` not implemented type", typeName)
 	}
